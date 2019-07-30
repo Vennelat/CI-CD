7405171a01bcd46bd636377d6d928a69fbadc417
Vennelat <50332925+Vennelat@users.noreply.github.com>
2019-07-29 19:17:12 -0700

    Update tfbackend.tf

--- terraform/dev/tfbackend.tf
+++ terraform/dev/tfbackend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-formac1"
+    bucket  = "terraformformac1"
     key     = "dev.json"
     region  = "us-west-2"
-    profile = "profile"
+    profile = "default"
   }
 }
